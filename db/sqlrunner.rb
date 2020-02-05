035241c3c7034d2b08232a64b3877d93057e31fe
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-02-05 11:54:41 +0000

    heroku postgres added

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -4,7 +4,12 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'budget', host: 'localhost'})
+      db = PG.connect({dbname: 'ddbrgmsbqm7114',
+        host: 'ec2-54-197-34-207.compute-1.amazonaws.com',
+        port: 5432,
+        user: 'wpekypzxxkenpp',
+        password: '84370ec1608601706f2e1289f196b7aef3aa905416fd7ecacc66a412c70be03e'
+        })
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
